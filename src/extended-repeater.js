3a08884df407ed27fa32467a82f18478196963f8
Leonid Shuliak <zeleny777@gmail.com>
2020-11-01 14:02:06 +0300

    fix: repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,29 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let addition = '';
+  if ('addition' in options)
+  {
+    addition += options.addition;
+    if ('additionRepeatTimes' in options)
+    {
+      for (let i = 1; i < options.additionRepeatTimes; ++i)
+      {
+        addition += options.additionSeparator || '|';
+        addition += options.addition;
+      }
+    }
+  }
+
+  let repeatTimes  = str + addition;
+  if ('repeatTimes' in options)
+  {
+    for (let i = 1; i < options.repeatTimes; ++i)
+    {
+      repeatTimes  += options.separator || '+';
+      repeatTimes  += str + addition;
+    }
+  }
+  return repeatTimes ;
 };
   
